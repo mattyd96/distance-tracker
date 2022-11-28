960bed39af6828d720453fbb9969f41bdce403f5
mattylume <matthew.durflinger@weareilume.com>
2022-11-29 10:36:41 +1100

    first commit wew

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let locationPing;
+  let totalDistance = 0;
+
+  const calculateDistance = (lat1, lon1, lat2, lon2) => {
+    const R = 6371e3; // metres
+    const φ1 = lat1 * Math.PI/180; // φ, λ in radians
+    const φ2 = lat2 * Math.PI/180;
+    const Δφ = (lat2-lat1) * Math.PI/180;
+    const Δλ = (lon2-lon1) * Math.PI/180;
+
+    const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+              Math.cos(φ1) * Math.cos(φ2) *
+              Math.sin(Δλ/2) * Math.sin(Δλ/2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+    const d = R * c; // in metres
+    return d;
+  }
+
+  const handleStart = () => {
+    let lastLon, lastLat;
+
+    locationPing = setInterval(() => {
+      navigator.geolocation.getCurrentPosition(data => {
+        //console.log(data)
+        if(lastLon && lastLat) {
+          totalDistance += calculateDistance(lastLat, lastLon, data.coords.latitude, data.coords.longitude);
+          console.log(totalDistance)
+        }
+        lastLat = data.coords.latitude
+        lastLon = data.coords.longitude
+      })
+    },1000)
+  };
+
+  const handleEnd = () => {
+    if(locationPing) {
+      clearInterval(locationPing)
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Your totalDistance is { totalDistance } metres</p>
+      <button onClick={handleStart}>Start Distance Tracking</button>
+      <button onClick={handleEnd}>Stop Distance Tracking</button>
     </div>
   );
 }
